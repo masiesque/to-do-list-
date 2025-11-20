69e51322813a323b221b7ffe14715023230f478a
Breno Masiesque <masiesquebreno@gmail.com>
2025-11-20 14:21:22 -0300

    +rotas

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,7 +4,7 @@ const Task = require('../models/task');
 const getAllTask = async (req, res)=>{
     try{
         const tasklist = await Task.find();
-        return res.render("index", {tasklist});
+        return res.render("index", {tasklist, task:null, taskDelete : null});
 
     }catch(err)
     {
@@ -25,11 +25,49 @@ const createTask = async (req, res) => {
         console.error(err);
         return res.status(500).json({ error: "Requisição não encontrada." });
     }
+};
+const getById = async (req, res) => {
+    try 
+    {
+        const {task, taskDelete} = await Task.findOne({ _id: req.params.id });
+        const tasklist = await Task.find();
+        if(req.params.method == uptade)
+        {
+             return res.render("index", { tasklist, task, taskDelete: null });
+        }else
+        {
+             return res.render("index", { tasklist, task: null, taskDelete });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "Erro ao encontrar ou renderizar a página" });
+    }
 }
+const updateOneTask = async (req, res)=>
+{
+    try{
+        const task = req.body;
+        await Task.updateOne({_id:req.params.id}, task);
+        res.redirect('/');
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({error: err.message});
+    }
+}
+const deleteTask = async (req, res)=>
+{
+    try{
+        const task = 
 
+    }catch(err)
+    {
+        onsole.log(err);
+        return res.status(500).json({error: err.message});
+    }
+}
 
 module.exports ={
     getAllTask,
     createTask,
-
+    getById,
+   updateOneTask,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,8 @@ const metodosController = require("../controllers/taskController");
 
 routes.get('/',metodosController.getAllTask );
 routes.post('/create',metodosController.createTask);
+routes.get('/getById/:id', metodosController.getById);
+routes.post('/update/:id', metodosController.updateOneTask)
 
 module.exports= routes;
 
